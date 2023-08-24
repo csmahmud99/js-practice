522754191e81e333aa5e435bfd826a70ad209825
csmahmud99 <csmahmud.2021@gmail.com>
2023-08-24 15:40:00 +0600

    Variable example with String Data Type

--- variables.js
+++ variables.js
@@ -2,3 +2,10 @@
 var price = 11;
 var age = 17;
 var temperature = 38;
+
+
+
+// Example of Variables :: Variable with String Data Type
+var person = "Sodor Uddin"; // String of Double Quotes
+var location = "Bandorban"; // String of Double Quotes
+var special = 'Alia Vhatt'; // String of Single Quotes

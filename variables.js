b3ee262df590bfd8e58350646c9c07dfbfdec7d8
csmahmud99 <csmahmud.2021@gmail.com>
2023-08-24 15:46:48 +0600

    Variable example with Boolean Data Type

--- variables.js
+++ variables.js
@@ -1,11 +1,26 @@
-// Example of Variables :: Variable with Numeric/Number Data Type
+// Example of Variables :: Variable with 'Numeric/Number' Data Type
 var price = 11;
 var age = 17;
 var temperature = 38;
 
 
 
-// Example of Variables :: Variable with String Data Type
+// Example of Variables :: Variable with 'String' Data Type
 var person = "Sodor Uddin"; // String of Double Quotes
 var location = "Bandorban"; // String of Double Quotes
 var special = 'Alia Vhatt'; // String of Single Quotes
+
+
+
+// Example of Variables :: Variables with 'Boolean' Data Types
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
+
+
+
+// Example of Variables :: Variables with 'Undefined' Data Types
+var isGuilty;   // Variable is declared, but value is not defined yet
+var evenNumber; // Variable is declared, but value is not defined yet
+console.log("var isGuilty:", isGuilty);      // Output: undefined
+console.log("var evenNumber:", evenNumber);    // Output: undefined

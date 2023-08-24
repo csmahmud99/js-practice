1a8665a2258ccfe0a98edcff07ce251ea5687730
csmahmud99 <csmahmud.2021@gmail.com>
2023-08-24 23:19:29 +0600

    Basic Mathematical Operation in JS

---
+++ operation.js
@@ -0,0 +1,35 @@
+// Addition Operation in JS without using variable.
+
+var onionPrice = 20;
+var eggPrice = 10;
+
+console.log("Onion Price:", onionPrice);    // Simply, Output: Onion Price: 20
+console.log("Egg Price", eggPrice);         // Simply, Output: Onion Price: 10
+
+console.log("Total Price:", onionPrice + eggPrice);     // Without using variable
+
+
+// Addition Operation using Variable
+var totalPrice = onionPrice + eggPrice;
+console.log("Total Price after Addition:", totalPrice); 
+
+
+// Subtraction Operation using Variable
+var priceDifference = onionPrice - eggPrice;
+console.log("Price Difference between Onion and Egg", priceDifference);
+
+
+// Multiplication Operation using Variable
+var orangePrice = 20;
+var quantity = 7;
+
+var totalCost = orangePrice * quantity;
+console.log("The Total Cost of 20 Oranges:", totalCost);
+
+
+// Division Operation using Variable
+var money = 500;
+var player = 10;
+
+var eachPlayer = money / player;
+console.log("Each Player will Get:", eachPlayer);

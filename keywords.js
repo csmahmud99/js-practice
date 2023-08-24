154fbae838cc266ec4fff8cde6a9f1fc6e9254d6
csmahmud99 <csmahmud.2021@gmail.com>
2023-08-24 16:01:20 +0600

    JavaScript Keywords

---
+++ keywords.js
@@ -0,0 +1,18 @@
+/* 
+// Keywords can not be written as variable's name in JavaScript
+
+var false = "peter";    // This is Incorrect
+var for = 15;           // This is Incorrect
+
+
+
+// JavaScript Keywords are case-sensitive
+
+var myName = "John";    // Correct
+VAR myName = "John";    // Incorrect use
+Var myName = "John";    // Incorrect use
+vaR myName = "John";    // Incorrect use
+vAr myName = "John";    // Incorrect use
+
+// 'var', 'VAR', 'Var', 'vaR' and 'vAr' - all are different from each other
+*/

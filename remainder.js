2e6e1fa008534d4a419da3be8d17dd79e3a39569
csmahmud99 <csmahmud.2021@gmail.com>
2023-08-25 02:25:58 +0600

    Remainder --- Done

---
+++ remainder.js
@@ -0,0 +1,10 @@
+var mangoes = 11;
+var person = 2;
+
+var divisionResult = mangoes / person; // Division
+
+console.log("Division Result:", divisionResult);
+
+var remainderResult = mangoes % person; // Remainder
+
+console.log("Remainder Result:", remainderResult);

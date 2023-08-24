95656083df1c65d869c9bd2c438da3f0c05a5944
csmahmud99 <csmahmud.2021@gmail.com>
2023-08-25 00:25:47 +0600

    String Concatenation

---
+++ concatenation.js
@@ -0,0 +1,23 @@
+// Addition (Concatenation) of 2 Strings without any space between them
+
+var firstName = "Jennie";
+var lastName = "Kim";
+
+var fullNameWithoutGap = firstName + lastName;
+console.log("Without Space:", fullNameWithoutGap); 
+
+
+// Addition (Concatenation) of 2 Strings with space between them
+
+var fullNameWithGap = firstName + " " + lastName;
+console.log("With Space:", fullNameWithGap); 
+
+/* ================================================================== */
+
+// Addition of 2 Numbers, among them one is in the String format
+
+var priceOne = "31";
+var priceTwo = 10;
+
+var totalPrice = priceOne + priceTwo;
+console.log("Total Price (Concatenation):", totalPrice); // Output : 3110

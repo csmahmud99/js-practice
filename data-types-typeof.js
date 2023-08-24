e6a7aeec066e1c373d1fd022f1889b62abfbd5cb
csmahmud99 <csmahmud.2021@gmail.com>
2023-08-25 01:33:07 +0600

    README file updated

---
+++ data-types-typeof.js
@@ -0,0 +1,20 @@
+// Data Type Checking
+
+/* ======== Numeric/Number Type Data :: Check by 'typeof' ======== */
+var price = 100;
+console.log(typeof price);
+
+
+/* ======== String Type Data :: Check by 'typeof' ======== */
+var newPrice = "100";
+console.log(typeof newPrice);
+
+
+/* ======== Boolean Type Data :: Check by 'typeof' ======== */
+var isHappy = true;
+console.log(typeof isHappy);
+
+
+/* ======== Undefined Type Data :: Check by 'typeof' ======== */
+var romantic;
+console.log(typeof romantic);

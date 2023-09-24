e18706955c73a4a740b6d6b8e1438b7362f3d66a
csmahmud99 <csmahmud.2021@gmail.com>
2023-09-24 21:31:41 +0600

    Finding an element of an array by it's 'Index (Position) Number'

--- allAboutArray.js
+++ allAboutArray.js
@@ -10,10 +10,20 @@ console.log("The output of the array 'vowels' is:", vowels);
 
 
 /* 
-// We can find the 'length' of an array in JavaScript; it means, we can see in the output how much elements an array carry
+// We can find the 'Length' of an array in JavaScript; it means, we can see in the output how much elements an array carry
 
 var vowels = ["a", "e", "i", "o", "u"];
 console.log("The length of the array 'vowels' is:", vowels.length);
 
 // Output || The length of the array 'vowels' is: 5 
 */
+
+
+
+// Finding an element of an array by it's 'Index (Position) Number'
+// Note: Index number of any array is begun with Zero (0).
+
+var numbers = [45, 68, 78, 56, 89, 98];
+console.log("Element of index number 1 of the array 'numbers' is:", numbers[1]);
+
+// Output || Element of index number 1 of the array 'numbers' is: 68

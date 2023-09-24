5b01a08bac3a7bdeb635983c1223b3dd0aab85af
csmahmud99 <csmahmud.2021@gmail.com>
2023-09-25 01:42:06 +0600

    Array Push and Array Pop

---
+++ arrayPushPop.js
@@ -0,0 +1,46 @@
+/* 
+// Array Push
+var numbers = [78, 45, 98, 45];
+console.log("Main array (numbers):", numbers);
+// Output || Main array (numbers): [ 78, 45, 98, 45 ]
+console.log("Length of the main array (numbers):", numbers.length);
+// Output || Length of the main array (numbers): 4
+
+
+// Adding a new element in the last of the main array (numbers) using 'push()' method
+numbers.push(63);
+console.log("The array (numbers) after adding an element in the last:", numbers);
+// Output || The array (numbers) after adding an element in the last: [ 78, 45, 98, 45, 63 ]
+console.log("New length of the array (numbers) after adding an element in the last:", numbers.length);
+// Output || New length of the array (numbers) after adding an element in the last: 5
+
+// We can also add more than one element in the last of an array using 'push()' method
+numbers.push(65, 66);
+console.log("The array (numbers) after adding more than one element in the last:", numbers);
+// Output || The array (numbers) after adding more than one element in the last: [ 78, 45, 98, 45, 63, 65, 66 ]
+console.log("New length of the array (numbers) after adding more than one element in the last:", numbers.length);
+// Output || New length of the array (numbers) after adding more than one element in the last: 7 
+*/
+
+
+// Array Pop
+var numbers = [78, 45, 98, 45, 63, 65, 66];
+console.log("Main array (numbers):", numbers);
+// Output || Main array (numbers): [ 78, 45, 98, 45, 63, 65, 66 ]
+console.log("Length of the main array (numbers):", numbers.length);
+// Output || Length of the main array (numbers): 7
+
+
+// Removing a new element from the last of the main array (numbers) using 'pop()' method
+numbers.pop();
+console.log("The array (numbers) after removing an element from the last:", numbers);
+// Output || The array (numbers) after removing an element from the last: [ 78, 45, 98, 45, 63, 65 ]
+console.log("New length of the array (numbers) after removing an element from the last:", numbers.length);
+// Output || New length of the array (numbers) after removing an element from the last: 6
+
+
+// Array 'pop()' method returns us the removed element from the array, which we can use later to anywhere by storing it inside a variable
+var numbersNew = [78, 45, 63, 65];
+var itemOfNumbers = numbersNew.pop();
+console.log("Removed item as return:", itemOfNumbers);
+// Output || Removed item as return: 65

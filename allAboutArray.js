c45d597a3793ecc3c5fd63f7ca57c18e5b7cd1a4
csmahmud99 <csmahmud.2021@gmail.com>
2023-09-24 21:45:04 +0600

    Finding an element of an array by it's 'Index (Position) Number'

--- allAboutArray.js
+++ allAboutArray.js
@@ -20,10 +20,24 @@ console.log("The length of the array 'vowels' is:", vowels.length);
 
 
 
+
 // Finding an element of an array by it's 'Index (Position) Number'
 // Note: Index number of any array is begun with Zero (0).
 
 var numbers = [45, 68, 78, 56, 89, 98];
-console.log("Element of index number 1 of the array 'numbers' is:", numbers[1]);
+console.log("Element of index number 0 (First Element) of the array 'numbers' is:", numbers[0]);
+console.log("Element of index number 1 (Second Element) of the array 'numbers' is:", numbers[1]);
+console.log("Element of index number 2 (Third Element) of the array 'numbers' is:", numbers[2]);
+console.log("Element of index number 3 (Fourth Element) of the array 'numbers' is:", numbers[3]);
 
-// Output || Element of index number 1 of the array 'numbers' is: 68
+// Output || Element of index number 0 (First Element) of the array 'numbers' is: 45 
+// Output || Element of index number 1 (Second Element) of the array 'numbers' is: 68 
+// Output || Element of index number 2 (Third Element) of the array 'numbers' is: 78 
+// Output || Element of index number 3 (Fourth Element) of the array 'numbers' is: 56 
+
+
+// This can be shown by storing it into a variable
+var fifthItem = numbers[4];
+console.log("Element of index number 4 (Fifth Element) of the array 'numbers' is:", fifthItem);
+
+// Output || Element of index number 4 (Fifth Element) of the array 'numbers' is: 89 

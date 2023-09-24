bb667f4c3db61f447e30ed3ba8778b8b83115aba
csmahmud99 <csmahmud.2021@gmail.com>
2023-09-25 02:19:41 +0600

    Array Unshift and Array Shift :: Comments Modified

--- arrayPushPop.js
+++ arrayPushPop.js
@@ -9,17 +9,17 @@ console.log("Length of the main array (numbers):", numbers.length);
 
 // Adding a new element to the last of the main array (numbers) using 'push()' method
 numbers.push(63);
-console.log("The array (numbers) after adding an element to the last:", numbers);
-// Output || The array (numbers) after adding an element to the last: [ 78, 45, 98, 45, 63 ]
-console.log("New length of the array (numbers) after adding an element to the last:", numbers.length);
-// Output || New length of the array (numbers) after adding an element to the last: 5
+console.log("The array (numbers) after adding a new element to the last:", numbers);
+// Output || The array (numbers) after adding a new element to the last: [ 78, 45, 98, 45, 63 ]
+console.log("New length of the array (numbers) after adding a new element to the last:", numbers.length);
+// Output || New length of the array (numbers) after adding a new element to the last: 5
 
-// We can also add more than one element to the last of an array using 'push()' method
+// We can also add more than one new element to the last of an array using 'push()' method
 numbers.push(65, 66);
-console.log("The array (numbers) after adding more than one element to the last:", numbers);
-// Output || The array (numbers) after adding more than one element to the last: [ 78, 45, 98, 45, 63, 65, 66 ]
-console.log("New length of the array (numbers) after adding more than one element to the last:", numbers.length);
-// Output || New length of the array (numbers) after adding more than one element to the last: 7 
+console.log("The array (numbers) after adding more than one new element to the last:", numbers);
+// Output || The array (numbers) after adding more than one new element to the last: [ 78, 45, 98, 45, 63, 65, 66 ]
+console.log("New length of the array (numbers) after adding more than one new element to the last:", numbers.length);
+// Output || New length of the array (numbers) after adding more than one new element to the last: 7 
 */
 
 
@@ -32,7 +32,7 @@ console.log("Length of the main array (numbers):", numbers.length);
 // Output || Length of the main array (numbers): 7
 
 
-// Removing a new element from the last of the main array (numbers) using 'pop()' method
+// Removing an element from the last of the main array (numbers) using 'pop()' method
 numbers.pop();
 console.log("The array (numbers) after removing an element from the last:", numbers);
 // Output || The array (numbers) after removing an element from the last: [ 78, 45, 98, 45, 63, 65 ]
@@ -40,7 +40,7 @@ console.log("New length of the array (numbers) after removing an element from th
 // Output || New length of the array (numbers) after removing an element from the last: 6
 
 
-// Array 'pop()' method returns us the removed element from the array, which we can use later to anywhere by storing it inside a variable
+// The array 'pop()' method returns us the removed element from the array, which we can use later anywhere by storing it inside a variable.
 var numbersNew = [78, 45, 63, 65];
 var itemOfNumbers = numbersNew.pop();
 console.log("Removed item as return:", itemOfNumbers);
--- arrayUnshiftShift.js
+++ arrayUnshiftShift.js
@@ -9,17 +9,17 @@ console.log("Length of the main array (numbers):", numbers.length);
 
 // Adding a new element to the first of the main array (numbers) using 'unshift()' method
 numbers.unshift(2);
-console.log("The array (numbers) after adding an element to the first:", numbers);
-// Output || The array (numbers) after adding an element to the first: [ 2, 6, 8, 10 ]
-console.log("New length of the array (numbers) after adding an element to the first:", numbers.length);
-// Output || New length of the array (numbers) after adding an element to the first: 4
+console.log("The array (numbers) after adding a new element to the first:", numbers);
+// Output || The array (numbers) after adding a new element to the first: [ 2, 6, 8, 10 ]
+console.log("New length of the array (numbers) after adding a new element to the first:", numbers.length);
+// Output || New length of the array (numbers) after adding a new element to the first: 4
 
-// We can also add more than one element to the first of an array using 'unshift()' method
+// We can also add more than one new element to the first of an array using 'unshift()' method
 numbers.unshift(4, 5);
-console.log("The array (numbers) after adding more than one element to the first:", numbers);
-// Output || The array (numbers) after adding more than one element to the first: [ 4, 5, 2, 6, 8, 10 ]
-console.log("New length of the array (numbers) after adding more than one element to the first:", numbers.length);
-// Output || New length of the array (numbers) after adding more than one element to the first: 6 
+console.log("The array (numbers) after adding more than one new element to the first:", numbers);
+// Output || The array (numbers) after adding more than one new element to the first: [ 4, 5, 2, 6, 8, 10 ]
+console.log("New length of the array (numbers) after adding more than one new element to the first:", numbers.length);
+// Output || New length of the array (numbers) after adding more than one new element to the first: 6 
 */
 
 
@@ -33,7 +33,7 @@ console.log("Length of the main array (numbers):", numbers.length);
 // Output || Length of the main array (numbers): 5
 
 
-// Removing a new element from the first of the main array (numbers) using 'shift()' method
+// Removing an element from the first of the main array (numbers) using 'shift()' method
 numbers.shift();
 console.log("The array (numbers) after removing an element from the first:", numbers);
 // Output || The array (numbers) after removing an element from the first: [ 4, 6, 8, 10 ]
@@ -41,7 +41,7 @@ console.log("New length of the array (numbers) after removing an element from th
 // Output || New length of the array (numbers) after removing an element from the first: 4
 
 
-// Array 'shift()' method returns us the removed element from the array, which we can use later to anywhere by storing it inside a variable
+// The array 'shift()' method returns us the removed element from the array, which we can use later anywhere by storing it inside a variable.
 var numbersNew = [78, 45, 63, 65];
 var itemOfNumbers = numbersNew.shift();
 console.log("Removed item as return:", itemOfNumbers);

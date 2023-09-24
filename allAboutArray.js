9d3a52dc7ecc918fec92b7fe6c0e6ef3edf3ea05
csmahmud99 <csmahmud.2021@gmail.com>
2023-09-24 21:04:49 +0600

    Seeing the output of an Array

---
+++ allAboutArray.js
@@ -0,0 +1,11 @@
+/* 
+// How to see the output of an array in JavaScript
+
+var vowels = ["a", "e", "i", "o", "u"];
+console.log("The output of the array 'vowels' is:", vowels);
+
+// Output || The output of the array 'vowels' is: [ 'a', 'e', 'i', 'o', 'u' ] 
+*/
+
+
+

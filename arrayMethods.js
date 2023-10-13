2057f78ca3178de9a12eabb8711af665eaf0b19d
csmahmud99 <csmahmud.2021@gmail.com>
2023-10-13 17:51:27 +0600

    'parseInt()' and 'parseFloat()' in JavaScript

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,7 +1,7 @@
 // ******************** Useful Array Methods ********************
 
 /* 
-// 1. Array 'Push' Method
+// 1. Array 'push()' Method
 // Adds one or, more than one new items to the end of an array.
 // Returns the new length of the array.
 
@@ -20,7 +20,7 @@ console.log(newValue2);                  // returns: 9
 
 
 /*
-// 2. Array 'Pop' Method
+// 2. Array 'pop()' Method
 // Removes the last item from an array.
 // Returns what was removed.
 
@@ -39,26 +39,26 @@ console.log(newValue2);                 // returns: "E"
 
 
 /* 
-// 3. Array 'Unshift' Method
+// 3. Array 'unshift()' Method
 // Adds one or, more than one new items to the beginning of an array.
 // Returns the new length of the array.
 
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
 
-var newValue1 = stuff.unshift("X");         // For adding one item
+var newValue1 = stuff.unshift("X");         // For adding a single item
 console.log(stuff);                         // stuff = [ 'X', 'A', 'B', 'C', 'D', 'E', 'F' ]
 console.log(newValue1);                     // returns: 7
 
 var newValue2 = stuff.unshift("Y", "Z");    // For adding more than one items
-// console.log(stuff);                      // stuff = [ 'Y', 'Z', 'X', 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
-console.log(newValue2);                     // returns: 10 
+// console.log(stuff);                      // stuff = [ 'Y', 'Z', 'X', 'A', 'B', 'C', 'D', 'E', 'F' ]
+console.log(newValue2);                     // returns: 9 
 */
 
 
 
 /* 
-// 4. Array 'Shift' Method
+// 4. Array 'shift()' Method
 // Removes the first item from an array.
 // Returns what was removed.
 
@@ -77,21 +77,21 @@ console.log(newValue2);             // returns: "B"
 
 
 /* 
-// 5. Array 'Splice' Method (with single Argument)
+// 5. Array 'splice()' Method (With Single Argument)
 // Gets rid of the index item and after it.
 // Returns what was removed.
 
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
 
-var newValue = stuff.splice(2);      // With single Argument
+var newValue = stuff.splice(2);      // With single argument
 console.log(stuff);                  // stuff = [ 'A', 'B' ]
 console.log(newValue);               // returns: [ 'C', 'D', 'E', 'F' ] 
 */
 
 
-/* 
-// 5. Array 'Splice' Method (with 2 Arguments)
+/*  
+// 5. Array 'splice()' Method (With 2 Arguments)
 // 1st argument: Index item from which the removing operation will start.
 // 2nd argument: Determined how many items are removed in total (including the index).
 // Returns what was removed.
@@ -99,35 +99,35 @@ console.log(newValue);               // returns: [ 'C', 'D', 'E', 'F' ]
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
 
-var newValue = stuff.splice(2, 2);  // With 2 Arguments
+var newValue = stuff.splice(2, 2);  // With 2 arguments
 console.log(stuff);                 // stuff = [ 'A', 'B', 'E', 'F' ]
 console.log(newValue);              // returns: [ 'C', 'D' ] 
 */
 
 
 /* 
-// 5. Array 'Splice' Method (with 3 Arguments - no item is removed)
+// 5. Array 'splice()' Method (With 3 Arguments - No Item is Removed)
 // In this case: Inserts the item named 'Hello' into the index number '2' and shifts the rest items ahead (without removing any items/removing '0' items).
 // Returns what was removed.
 
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
 
-var newValue = stuff.splice(2, 0, "Hello");     // With 3 Arguments: no item is removed
+var newValue = stuff.splice(2, 0, "Hello");     // With 3 arguments: no item is removed
 console.log(stuff);                             // stuff = [ 'A', 'B', 'Hello', 'C', 'D', 'E', 'F' ]
 console.log(newValue);                          // returns: [] 
 */
 
 
 /* 
-// 5. Array 'Splice' Method (with 3 Arguments - some items are removed)
+// 5. Array 'splice()' Method (With 3 Arguments - Some Items are Removed)
 // In this case: Inserts the items named 'Hello' into the index number '2' and removes '3' items (including the index number '2' and after)
 // Returns what was removed.
 
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
 
-var newValue = stuff.splice(2, 3, "Hello");     // With 3 Arguments: some items are removed
+var newValue = stuff.splice(2, 3, "Hello");     // With 3 arguments: some items are removed
 console.log(stuff);                             // stuff = [ 'A', 'B', 'Hello', 'F' ]
 console.log(newValue);                          // returns: [ 'C', 'D', 'E' ] 
 */
@@ -135,21 +135,21 @@ console.log(newValue);                          // returns: [ 'C', 'D', 'E' ]
 
 
 /* 
-// 6. Array 'Slice' Method (with single Argument)
+// 6. Array 'slice()' Method (With Single Argument)
 // Almost same as the Array 'Splice' Method; but does not modify the original array.
 // Cuts off the index item and after it, then returns the cut-items.
 
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
 
-var newValue = stuff.slice(2);         // With single Argument
+var newValue = stuff.slice(2);         // With single argument
 console.log(stuff);                    // stuff = ["A", "B", "C", "D", "E", "F"]
 console.log(newValue);                 // returns: [ 'C', 'D', 'E', 'F' ]
 */
 
 
 /* 
-// 6. Array 'Slice' Method (with 2 Arguments)
+// 6. Array 'slice()' Method (With 2 Arguments)
 // Almost same as the Array 'Splice' Method; but does not modify the original array.
 // 1st argument indicates the index number of the item from which have to start the cut-off.
 // 2nd argument indicates the index number of the item before which we have to stop the cut off.
@@ -158,7 +158,7 @@ console.log(newValue);                 // returns: [ 'C', 'D', 'E', 'F' ]
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
 
-var newValue = stuff.slice(2, 4);       // With 2 Arguments
+var newValue = stuff.slice(2, 4);       // With 2 arguments
 console.log(stuff);                     // stuff = [ 'A', 'B', 'C', 'D', 'E', 'F' ]
 console.log(newValue);                  // returns: [ 'C', 'D' ] 
 */
@@ -166,7 +166,7 @@ console.log(newValue);                  // returns: [ 'C', 'D' ]
 
 
 /* 
-// 7. Array 'IndexOf' Method
+// 7. Array 'indexOf()' Method
 // Returns the index (position) number of the item/element in an array.
 
 // Array function
@@ -179,7 +179,7 @@ console.log(newValue);                  // returns: 2
 
 
 /* 
-// 8. Array 'Concat' Method
+// 8. Array 'concat()' Method
 // Returns the combination of any array (here, the array named 'stuff') with the array-argument of the array 'concat' method
 
 // Array function
@@ -192,7 +192,7 @@ console.log(newValue);                          // returns: [ 'A', 'B', 'C', D',
 
 
 /* 
-// 9. Array 'ToString' Method
+// 9. Array 'toString()' Method
 // Returns the combination of all array elements into only one single string
 
 // Array function
@@ -205,8 +205,8 @@ console.log(newValue);             // returns: "A,B,C,D,E,F"
 
 
 /* 
-// 10. Array 'Join' Method
-// Works almost the same as the Array 'ToString' Method, returns the combination of all array elements into only one single string; but the argument determines  what is between each element in the return-string
+// 10. Array 'join()' Method
+// Works almost the same as the Array 'ToString' Method; returns the combination of all array elements into only one single string; but the argument determines what is between each element in the return-string
 
 // Array function
 var stuff = ["A", "B", "C", "D", "E", "F"];  // Test array
@@ -218,7 +218,7 @@ console.log(newValue);             // returns: "A-B-C-D-E-F"
 
 
 /* 
-// 11. Array 'Reverse' Method
+// 11. Array 'reverse()' Method
 // Returns the same array in a reverse format (so, index number of the array elements will change)
 
 // Array function
---
+++ parse.js
@@ -0,0 +1,10 @@
+// 'parseInt()' and 'parseFloat()' Method in JavaScript
+
+var num1 = 70.78;
+var num2 = 70;
+
+var sum = num1 + num2;
+
+console.log(parseInt(sum));     // Output || 140
+
+console.log(parseFloat(sum));   // Output || 140.78

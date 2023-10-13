73432a62d89fd160b2d36dd68b9b107a084e1f51
csmahmud99 <csmahmud.2021@gmail.com>
2023-10-13 18:00:09 +0600

    Concat - problem for Addition Operation

--- concatenation.js
+++ concatenation.js
@@ -16,12 +16,23 @@ console.log("With Space:", fullNameWithGap);
 /* ================================================================== */
 
 // Addition of 2 Numbers, among them one is in the String format
+// Only for the 'Addition Operation', the problem of 'Concat' will happen.
+// For 'Subtraction/Multiplication/Division Operation', this problem of 'Concat' will not happen.
 
 var priceOne = "31";
 var priceTwo = 10;
 
 var totalPrice = priceOne + priceTwo;
 console.log("Total Price (Concatenation):", totalPrice); // Output : 3110
+
+var restPrice = priceOne - priceTwo;
+console.log("Total Price (Concatenation):", totalPrice); // Output : 21
+
+var multiplyPrice = priceOne * priceTwo;
+console.log("Total Price (Concatenation):", totalPrice); // Output : 310
+
+var dividedPrice = priceOne / priceTwo;
+console.log("Total Price (Concatenation):", totalPrice); // Output : 3.1
 
 /* ================================================================== */
 

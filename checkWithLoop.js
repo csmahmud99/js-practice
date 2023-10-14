db859cfb3917a09940554938df7d17e75537a92a
csmahmud99 <csmahmud.2021@gmail.com>
2023-10-15 03:16:33 +0600

    We can check any conditions using 'if/if-else' Conditionals within the 'for' loop.

---
+++ checkWithLoop.js
@@ -0,0 +1,30 @@
+/* 
+We can check any conditions using 'if/if-else' Conditionals within the 'for' loop.
+*/
+
+/* 
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (var i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
+    if (element == 5) {
+        console.log("Have got '5'");
+    }
+    console.log(element);
+} 
+*/
+
+
+/* 
+// Output ||
+1
+2
+3
+4
+Have got '5'
+5
+6
+7
+8
+9 
+*/

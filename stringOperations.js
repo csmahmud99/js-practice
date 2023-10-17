08df2e7b4f38c0d33f890a1b948472db11999b09
csmahmud99 <csmahmud.2021@gmail.com>
2023-10-17 14:18:16 +0600

    String 'includes' Method

---
+++ stringOperations.js
@@ -0,0 +1,11 @@
+// String Includes Method
+var myName = "Ariana Grande is singing";
+
+var isThere = myName.includes("is");
+console.log(isThere);           // Output || true
+
+var isThereName01 = myName.includes("Ariana");
+console.log(isThereName01);     // Output || true
+
+var isThereName02 = myName.includes("ariana");
+console.log(isThereName02);     // Output || false
